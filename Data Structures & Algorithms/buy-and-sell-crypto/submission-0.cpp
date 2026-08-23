e35723f2a25cabd12fb3f01fb65e4f4abedfdd5c
jswoo0615 <128343128+jswoo0615@users.noreply.github.com>
2026-08-24 07:34:16 +0900

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_price = prices[0];
+        int max_profit = 0;
+
+        for (int price : prices) {
+            if (price < min_price) {
+                min_price = price;
+            } else {
+                max_profit = std::max(max_profit, price - min_price);
+            }
+        }
+        return max_profit;
+    }
+};
